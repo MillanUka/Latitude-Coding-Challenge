9fa11b01d384a9aec5dd6f84ff626d0e2b3b3d0d
MillanUka <ukamillan@gmail.com>
2021-04-29 19:47:54 +1200

    Add more tests and the implementation of the algorithm
    Added too more test. One test for 1440, the number of minutes ina day. Also added an exhaustative search algorithm, which pass the tests. May need to make it more efficient later

--- src/coding/challenge/CodingChallenge.java
+++ src/coding/challenge/CodingChallenge.java
@@ -15,10 +15,26 @@ public class CodingChallenge {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] stockPrices = {10, 7, 5, 8, 11, 9};
+        System.out.println(getMaxProfit(stockPrices));
     }
-    
+
     public static int getMaxProfit(int[] stockPrices) {
-       return 0;
+        // Cannot make a trade if there is only 1 element in the array
+        if(stockPrices.length < 2) {
+            return 0;
+        }
+        int maxProfit =  stockPrices[1] - stockPrices[0];
+        for(int i =0; i < stockPrices.length; i++) {
+            int stockPrice = stockPrices[i];
+            for(int j =1+i; j < stockPrices.length; j++) {
+                int profit =  stockPrices[j] - stockPrice;
+                if(profit > maxProfit) {
+                    //System.out.println("Bought at $" + stockPrice + " sold at $" + stockPrices[j] );
+                    maxProfit = profit; 
+                }
+            }
+        }
+        return maxProfit;
     }
 }
