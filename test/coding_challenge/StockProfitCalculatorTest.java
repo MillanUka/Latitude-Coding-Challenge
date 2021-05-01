edba5fef1c1698085989c4b18ab159fb53e866fa
MillanUka <ukamillan@gmail.com>
2021-05-01 15:19:37 +1200

    A little bit of refactoring of the package names

--- test/coding_challenge/StockProfitCalculatorTest.java
+++ test/coding_challenge/StockProfitCalculatorTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package coding.challenge;
+package coding_challenge;
 
+import coding_challenge.StockProfitCalculator;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
